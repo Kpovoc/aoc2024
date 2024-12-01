23d4c68e1a4d319c15ad4c3507bfd8e8e66c87e5
Kpovoc <kpovoc@protonmail.com>
2024-12-01 13:27:41 -0600

    Day 01: 1/2

--- spec/day01/core_spec.clj
+++ spec/day01/core_spec.clj
@@ -3,5 +3,16 @@
             [day01.core :refer :all]))
 
 (describe "day01 test"
-  (it "FIXME, I fail."
-    (should= 1 1)))
+  (it "Sorts vectors ltg, takes the difference in each index, and sums them"
+      (let [l1 [3 4 2 1 3 3]
+            l2 [4 3 5 3 9 3]]
+        (should= 11 (distance-diff l1 l2)))))
+
+(describe
+  "line-list-to-num-lists"
+  (it "turns a list of lines into 2 lists of numbers"
+      (let [lines ["3425   564"
+                   "3   57"
+                   "678   45321"]]
+        (should= [[3425 3 678] [564 57 45321]]
+                 (line-list-to-num-lists lines)))))
