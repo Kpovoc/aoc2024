72ab26e03e7ce38015fc46cd0dc6fb2daabe3498
Kpovoc <kpovoc@protonmail.com>
2024-12-03 21:37:29 -0600

    Day 03: 1/2

--- spec/day03/core_spec.clj
+++ spec/day03/core_spec.clj
@@ -2,6 +2,35 @@
   (:require [speclj.core :refer :all]
             [day03.core :refer :all]))
 
-(describe "day03 test"
-  (it "FIXME, I fail."
-    (should= 1 1)))
+(describe
+  "parse-num"
+  (it
+    "returns 0 for the number, and the rest of the string from the starting character when invalid"
+    (should= {:num 0 :s "*"} (parse-num "4*" \,))
+    (should= {:num 0 :s ",9!"} (parse-num "6,9!" \)))
+    (should= {:num 0 :s "2,345]"} (parse-num "12,345]" \)))
+    (should= {:num 0 :s "2"} (parse-num "22" \)))
+    (should= {:num 0 :s "234"} (parse-num "1234" \)))
+    (should= {:num 0 :s "2 , 4 )"} (parse-num " 2 , 4 )" \,))
+    (should= {:num 0 :s ""} (parse-num "" \,))
+    (should= {:num 0 :s ""} (parse-num " " \,)))
+  (it
+    "returns number and rest of string after terminator"
+    (should= {:num 2 :s "4)%&mul[3,7]!"} (parse-num "2,4)%&mul[3,7]!" \,))
+    (should= {:num 43 :s "%&mul[3,7]!"} (parse-num "43)%&mul[3,7]!" \)))))
+
+(describe
+  "mult-args"
+  (it
+    "returns 0 for the number, and the rest of the string from the starting character when invalid"
+    (should= {:num 0 :s ",7]!@^do_not"} (mult-args "3,7]!@^do_not"))
+    (should= {:num 0 :s ")+mul"} (mult-args "5)+mul")))
+  (it
+    "returns the product and the rest of the string from the closing parenthesis"
+    (should= {:num 8 :s "%&mul["} (mult-args "2,4)%&mul["))
+    (should= {:num 25 :s "+mul(32,64]then("} (mult-args "5,5)+mul(32,64]then("))))
+
+(describe
+  "parse-mult-func"
+  (it "sums the products of the mul(x,y) functions"
+      (should= 161 (parse-mult-func "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))))
--- src/day03/core.clj
+++ src/day03/core.clj
@@ -1,4 +1,77 @@
 (ns day03.core)
 
+(def mul-fn "mul(")
+(def max-num-size 3)
+
+(defn is-num? [c]
+  (let [v (Character/getNumericValue ^char c)]
+    (and (>= v 0) (<= v 9))))
+
+(defn parse-num [s term]
+  (if (empty? s)
+    {:num 0 :s s}
+    (loop [rs s
+           digits ""
+           count 0]
+      (if (empty? rs)
+        {:num 0 :s (subs s 1)}
+        (let [c (first rs)
+              digit? (is-num? c)
+              term? (= c term)]
+          (cond
+            (and (= count max-num-size) (not term?))
+            {:num 0 :s (subs s 1)}
+
+            (and (= count 0) (not digit?))
+            {:num 0 :s (subs s 1)}
+
+            (and (not term?) (not digit?))
+            {:num 0 :s (subs s 1)}
+
+            term?
+            {:num (Integer/parseInt digits) :s (subs rs 1)}
+
+            digit?
+            (recur (subs rs 1) (str digits c) (inc count))))))))
+
+(defn mult-args [s]
+  (let [resp1 (parse-num s \,)
+        num1 (:num resp1)]
+    (if (= num1 0)
+      {:num 0 :s (subs s 1)}
+      (let [resp2 (parse-num (:s resp1) \))
+            num2 (:num resp2)]
+        (if (= num2 0)
+          {:num 0 :s (subs s 1)}
+          {:num (* num1 num2) :s (:s resp2)})))))
+
+(defn parse-mult-func [s]
+  (loop [mul mul-fn
+         rs s
+         nums []]
+    (cond
+      (empty? rs)
+      (reduce + nums)
+
+      :else
+      (let [c (first rs)
+            vc (first mul)]
+        (cond
+          (not= c vc)
+          (if (= (count mul) 4)
+            (recur mul-fn (subs rs 1) nums)
+            (recur mul-fn rs nums))
+
+          :else
+          (if (= (count mul) 1)
+            (let [res (mult-args (subs rs 1))]
+              (recur mul-fn (:s res) (conj nums (:num res))))
+            (recur (subs mul 1) (subs rs 1) nums)))))))
+
+; count 0, then c is 0-9
+; count 1, then c is 0-9 or ,
+; count 2, then c is 0-9 or ,
+; count 3, then c is ,
+
 (defn -main []
-  (println "Day 03"))
+  (println (parse-mult-func (slurp "data/day03"))))
